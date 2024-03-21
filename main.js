a2716e513b49d3de9d224b2c745e431fb99085ee
alphaleadership <thomas.iniguez@free.fr>
2024-03-21 16:20:47 +0100

    update

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const temp =document.createElement("div")
+const temp =Document.createElement("div")
 temp.innerText="ceci est une div ajoutée par texte"
-document.getElementById('content').appendChild(temp)
+Document.getElementById('content').appendChild(temp)
