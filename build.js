fdbcdac92b70bd68b04ea370ce93dff961ebc546
alphaleadership <thomas.iniguez@free.fr>
2024-04-11 17:04:47 +0200

    patch

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs =require("fs");
 const b64=require("node:crypto")
 console.log(b64)
 fs.appendFileSync("./main.js",`const temp =document.createElement("div")
-temp.innerText="ceci est une div ajoutée par texte"
+temp.innerHTML="ceci est une div ajoutée par texte"
 temp.innerHTML+="<img src=\\\"base64:`)
 fs.appendFileSync("./main.js",fs.readFileSync('./img.svg').toString("base64"))
 fs.appendFileSync("./main.js",">\"document.getElementById('content').appendChild(temp)")
